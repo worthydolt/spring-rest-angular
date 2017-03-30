ed32c9b11167fb6d9a8c738de0c1ca92330ee2f7
worthydolt <neil.frow@gmail.com>
2017-03-30 12:20:48 +0100

    Removed redundant template and convert number of ratings value to percentage of total

--- src/main/resources/static/app/shared-data/shared-data.component.js
+++ src/main/resources/static/app/shared-data/shared-data.component.js
@@ -3,6 +3,16 @@ angular.module('iWorksRatingsApp').factory('sharedData', function () {
 
     self.establishments = {};
 
+    self.totalRatings = function(){
+        var totalRating = 0;
+        for (var key in self.establishments){
+            if (self.establishments.hasOwnProperty(key)){
+                totalRating += self.establishments[key];
+            }
+        }
+        return totalRating;
+    }
+
     return self;
 
 })
--- src/main/resources/static/app/view-ratings/view-ratings.component.js
+++ src/main/resources/static/app/view-ratings/view-ratings.component.js
@@ -2,17 +2,17 @@ angular.module('viewRatings').component('viewRatings', {
     templateUrl: 'view-ratings/view-ratings.template.html',
     controller: ['$scope', 'sharedData',
         function ViewRatingsController($scope, sharedData) {
-            $scope.establishments = sharedData.establishments;
+            $scope.$watch(function() {
+                    $scope.establishments = sharedData.establishments;
+                    $scope.totalRatings = sharedData.totalRatings();
+                },
 
-            $scope.$watch(function() { $scope.establishments = sharedData.establishments; },
-
-                function(newValue, oldValue) {
-                    if (newValue !== oldValue){
-                        console.log('changed scope establishments value');
-                    }
+            function(newValue, oldValue) {
+                if (newValue !== oldValue){
+                    console.log('changed scope establishments value');
                 }
-            );
-            console.log(sharedData.establishments);
+            }
+        );
         }
     ]
 })
